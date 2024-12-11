b1a748196bb0ab8c6489b316670a247b6495e30c
bgaudino <bgaudino@gmail.com>
2024-12-10 22:01:43 -0600

    Day 9 part 2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -48,9 +48,65 @@ fn calculate_checksum(disk_map: Vec<String>) -> usize {
         .sum()
 }
 
+fn find_space(disk_map: &Vec<String>, size: &usize) -> Option<usize> {
+    let mut start: Option<usize> = None;
+    for (i, char) in disk_map.iter().enumerate() {
+        if char == "." {
+            if start.is_none() {
+                start = Some(i);
+            }
+        } else {
+            if let Some(s) = start {
+                if i - s >= *size {
+                    return Some(s);
+                } else {
+                    start = None
+                }
+            } else {
+                start = None
+            }
+        }
+    }
+    None
+}
+
+fn move_files(disk_map: &mut Vec<String>) {
+    let mut current: Option<&str> = None;
+    let mut end = disk_map.len() - 1;
+    for (i, ch) in disk_map.clone().iter().enumerate().rev() {
+        if let Some(id) = current {
+            if ch != id {
+                let start = i + 1;
+                let l = end - start;
+                if let Some(space) = find_space(&disk_map, &l) {
+                    if space < start {
+                        disk_map.splice(space..space + l, vec![id.to_string(); l]);
+                        disk_map.splice(start..end, vec![".".to_string(); l]);
+                    }
+                }
+                current = {
+                    if ch == "." {
+                        None
+                    } else {
+                        end = i + 1;
+                        Some(ch)
+                    }
+                }
+            }
+        } else if ch != "." {
+            end = i + 1;
+            current = Some(ch)
+        }
+    }
+}
+
 fn main() {
-    let disk_map = fs::read_to_string("input.txt").unwrap();
-    let mut expanded = expand(&disk_map);
-    move_blocks(&mut expanded);
-    println!("Part 1: {}", calculate_checksum(expanded));
+    let contents = fs::read_to_string("input.txt").unwrap();
+    let mut disk_map = expand(&contents);
+    move_blocks(&mut disk_map);
+    println!("Part 1: {}", calculate_checksum(disk_map));
+
+    disk_map = expand(&contents);
+    move_files(&mut disk_map);
+    println!("Part 2: {}", calculate_checksum(disk_map));
 }
