6b41d7eb48d2106465379e9738025f41f89a4f6f
bgaudino <bgaudino@gmail.com>
2024-12-13 15:34:10 -0600

    Day 13 part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,11 +1,8 @@
-use std::{collections::HashSet, fs};
-
 use regex::Regex;
+use std::fs;
 
-type Point = (usize, usize);
-type State = (Point, usize);
+type Point = (isize, isize);
 
-#[derive(Debug)]
 struct ClawMachine {
     a: Point,
     b: Point,
@@ -14,55 +11,50 @@ struct ClawMachine {
 
 fn parse_machine(s: &str) -> ClawMachine {
     let mut lines = s.lines();
-    let a = parse_point(lines.next().unwrap());
-    let b = parse_point(lines.next().unwrap());
-    let prize = parse_point(lines.next().unwrap());
-    ClawMachine{
-        a, b, prize
-    }
+    let a = parse_point(lines.next().expect("Missing A button"));
+    let b = parse_point(lines.next().expect("Missing B button"));
+    let prize = parse_point(lines.next().expect("Missing prize"));
+    ClawMachine { a, b, prize }
 }
 
 fn parse_point(line: &str) -> Point {
     let re = Regex::new(r"\d+").expect("Invalid regex");
     let mut digits = re
         .find_iter(line)
-        .map(|m| m.as_str().parse().unwrap());
-    let x = digits.next().unwrap();
-    let y = digits.next().unwrap();
+        .map(|m| m.as_str().parse::<isize>().expect("Invalid number"));
+    let x = digits.next().expect("Missing X coordinate");
+    let y = digits.next().expect("Missing Y coordinate");
     (x, y)
 }
 
-fn lowest_cost(machine: &ClawMachine) -> usize {
-    let mut queue: Vec<State> = vec![((0, 0), 0)];
-    let mut lowest_cost: Option<usize> = None;
-    let mut seen: HashSet<State> = HashSet::new();
-    while !queue.is_empty() {
-        let state = queue.remove(0);
-        if seen.contains(&state) {
-            continue;
-        }
-        seen.insert(state);
-        let (claw, cost) = state;
-        if claw.0 > machine.prize.0 || claw.1 > machine.prize.1 {
-            continue;
-        }
-        if let Some(lowest) = lowest_cost {
-            if cost >= lowest {
-                continue;
-            } 
-        }
-        if claw == machine.prize {
-            lowest_cost = Some(cost);
-        }
-        let push_b = ((claw.0 + machine.b.0, claw.1 + machine.b.1), cost + 1);
-        let push_a = ((claw.0 + machine.a.0, claw.1 + machine.a.1), cost + 3);
-        queue.extend(vec![push_b, push_a]);
+fn lowest_cost(machine: &ClawMachine, extra: isize) -> isize {
+    let (ax, ay) = machine.a;
+    let (bx, by) = machine.b;
+    let (px, py) = (machine.prize.0 + extra, machine.prize.1 + extra);
+
+    let d = ax * by - ay * bx;
+    let a = by * px - bx * py;
+    let b = ax * py - ay * px;
+
+    if a % d != 0 || b % d != 0 {
+        return 0;
     }
-    lowest_cost.unwrap_or(0)
+
+    let a_presses = a / d;
+    let b_presses = b / d;
+    let cost = 3 * a_presses + b_presses;
+
+    cost
+}
+
+fn solve(machines: &[ClawMachine], extra: isize) -> isize {
+    machines.iter().map(|m| lowest_cost(m, extra)).sum()
 }
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Could not read file");
-    let part_1: usize = contents.split("\n\n").map(|s| lowest_cost(&parse_machine(s))).sum();
-    println!("Part 1: {}", part_1);
+    let machines: Vec<ClawMachine> = contents.split("\n\n").map(|s| parse_machine(s)).collect();
+
+    println!("Part 1: {}", solve(&machines, 0));
+    println!("Part 2: {}", solve(&machines, 10_000_000_000_000));
 }
