4fe0c1b2f2604f3f8b775232747192b56d211767
bgaudino <bgaudino@gmail.com>
2024-12-13 10:33:14 -0600

    Day 12 part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -3,23 +3,24 @@ use std::{
     fs,
 };
 
-static DIRECTIONS: [(i32, i32); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+type Point = (i32, i32);
+type Side = (Point, Point);
 
-fn measure(
-    map: &HashMap<(i32, i32), char>,
-    point: (i32, i32),
-    ch: char,
-) -> (HashSet<(i32, i32)>, i32) {
-    let (mut area, mut perimeter) = (0, 0);
-    let mut plot: HashSet<(i32, i32)> = HashSet::new();
-    let mut stack: Vec<(i32, i32)> = vec![point];
+static HORIZONTAL: [Point; 2] = [(0, -1), (0, 1)];
+static VERTICAL: [Point; 2] = [(1, 0), (-1, 0)];
+static DIRECTIONS: [Point; 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+
+fn measure(map: &HashMap<Point, char>, point: Point, ch: char) -> (HashSet<Point>, HashSet<Side>, usize) {
+    let mut plot: HashSet<Point> = HashSet::new();
+    let mut fence: HashSet<Side> = HashSet::new();
+    let mut stack: Vec<Point> = vec![point];
+    let mut perimeter = 0;
     while !stack.is_empty() {
         let point = stack.pop().unwrap();
         if plot.contains(&point) {
             continue;
         }
         plot.insert(point);
-        area += 1;
         for direction in DIRECTIONS {
             let next_point = (point.0 + direction.0, point.1 + direction.1);
             let next_ch = map.get(&next_point).unwrap_or(&' ');
@@ -28,16 +29,43 @@ fn measure(
                     stack.push(next_point);
                 }
             } else {
+                fence.insert((point, direction));
                 perimeter += 1;
             }
         }
     }
-    (plot, area * perimeter)
+    (plot, fence, perimeter)
+}
+
+fn follow_side(fence: &HashSet<Side>, side: Side) -> HashSet<Side> {
+    let mut sides: HashSet<Side> = HashSet::new();
+    let mut stack: Vec<Side> = vec![side];
+    while !stack.is_empty() {
+        let (side, direction) = stack.pop().unwrap();
+        if sides.contains(&(side, direction)) {
+            continue;
+        }
+        sides.insert((side, direction));
+        let directions = {
+            if direction.0 == 0 {
+                VERTICAL
+            } else {
+                HORIZONTAL
+            }
+        };
+        for new_direction in directions {
+            let new_side = ((side.0 + new_direction.0, side.1 + new_direction.1), direction);
+            if fence.contains(&new_side) {
+                stack.push(new_side);
+            }
+        }
+    }
+    sides 
 }
 
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
-    let mut map: HashMap<(i32, i32), char> = HashMap::new();
+    let mut map: HashMap<Point, char> = HashMap::new();
 
     for (y, row) in contents.lines().enumerate() {
         for (x, ch) in row.chars().enumerate() {
@@ -46,15 +74,28 @@ fn main() {
         }
     }
 
-    let mut part_1 = 0;
-    let mut seen: HashSet<(i32, i32)> = HashSet::new();
+    let (mut part_1, mut part_2) = (0, 0);
+    let mut seen: HashSet<Point> = HashSet::new();
     for (point, ch) in &map {
         if seen.contains(point) {
             continue;
         }
-        let (plot, price) = measure(&map, *point, *ch);
-        seen.extend(plot);
-        part_1 += price;
+        let (plot, fence, perimeter) = measure(&map, *point, *ch);
+        seen.extend(&plot);
+        part_1 += plot.len() * perimeter;
+
+        let mut sides: HashSet<Side> = HashSet::new();
+        let mut s = 0;
+        for segment in &fence {
+            if sides.contains(&segment) {
+                continue;
+            }
+            let side = follow_side(&fence, *segment);
+            sides.extend(&side);
+            s += 1;
+        }
+        part_2 += plot.len() * s;
     }
     println!("Part 1: {}", part_1);
+    println!("Part 2: {}", part_2);
 }
