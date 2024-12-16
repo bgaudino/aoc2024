6b4b63bd5c5459d32b0d8b1337412a50e238d9ff
bgaudino <bgaudino@gmail.com>
2024-12-15 22:56:58 -0600

    Day 15 part 2 wip wrong

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -3,26 +3,63 @@ use std::{collections::HashMap, fs};
 type Point = (i32, i32);
 type Warehouse = HashMap<Point, char>;
 
-static LANTERNFISH: char = '@';
-static SPACE: char = '.';
-static WALL: char = '#';
-static BOX: char = 'O';
-
-fn get_direction(ch: char) -> Point {
-    match ch {
-        '^' => (0, -1),
-        'v' => (0, 1),
-        '<' => (-1, 0),
-        '>' => (1, 0),
-        _ => {
-            panic!("Invalid direction")
+fn get_warehouse(s: &str) -> (Warehouse, Point) {
+    let mut location: Point = (-1, -1);
+    let mut warehouse: Warehouse = Warehouse::new();
+    for (y, row) in s.lines().enumerate() {
+        for (x, ch) in row.chars().enumerate() {
+            warehouse.insert((x as i32, y as i32), ch);
+            if ch == '@' {
+                location = (x as i32, y as i32);
+            }
         }
     }
+    (warehouse, location)
+}
+
+fn get_directions(s: &str) -> Vec<Point> {
+    s.chars()
+        .filter(|c| c != &'\n')
+        .map(|ch| match ch {
+            '^' => (0, -1),
+            'v' => (0, 1),
+            '<' => (-1, 0),
+            '>' => (1, 0),
+            _ => {
+                panic!("Invalid direction")
+            }
+        })
+        .collect()
+}
+
+fn gps(warehouse: &Warehouse) -> i32 {
+    warehouse
+        .iter()
+        .map(|((x, y), ch)| {
+            if ch == &'O' || ch == &'[' {
+                100 * y + x
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+fn expand(s: &str) -> String {
+    s.chars()
+        .map(|c| match c {
+            '#' => "##",
+            'O' => "[]",
+            '@' => "@.",
+            '\n' => "\n",
+            _ => "..",
+        })
+        .collect()
 }
 
 fn _print_warehouse(warehouse: &Warehouse) {
-    for y in 0..10 {
-        for x in 0..10 {
+    for y in 0..50 {
+        for x in 0..100 {
             print!("{}", warehouse.get(&(x, y)).unwrap());
         }
         println!()
@@ -30,57 +67,58 @@ fn _print_warehouse(warehouse: &Warehouse) {
     println!()
 }
 
-fn try_move(warehouse: &mut Warehouse, location: &Point, direction: &Point) -> Point {
+fn try_move(
+    warehouse: &mut Warehouse,
+    location: &Point,
+    direction: &Point,
+    recurse: bool,
+) -> Point {
     let space = *warehouse.get(location).unwrap();
     let next_location = (location.0 + direction.0, location.1 + direction.1);
     let next_space = warehouse.get(&next_location).unwrap();
-    if *next_space == WALL {
+    if next_space == &'#' {
         return *location;
-    } else if *next_space != SPACE {
-        if next_location == try_move(warehouse, &next_location, direction) {
+    } else if next_space != &'.' {
+        if next_location == try_move(warehouse, &next_location, direction, true) {
             return *location;
         }
     }
-    warehouse.insert(*location, SPACE);
+
+    if direction.1 != 0 && recurse {
+        if space == '[' {
+            let partner_location = (location.0 + 1, location.1);
+            if partner_location == try_move(warehouse, &partner_location, direction, false) {
+                return *location;
+            }
+        } else if space == ']' {
+            let partner_location = (location.0 - 1, location.1);
+            if partner_location == try_move(warehouse, &partner_location, direction, false) {
+                return *location;
+            }
+        }
+    }
+
+    warehouse.insert(*location, '.');
     warehouse.insert(next_location, space);
     next_location
 }
-
 
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
     let mut segments = contents.split("\n\n");
 
-    let mut location: Point = (-1, -1);
-    let mut warehouse: Warehouse = Warehouse::new();
-    for (y, row) in segments.next().unwrap().lines().enumerate() {
-        for (x, ch) in row.chars().enumerate() {
-            warehouse.insert((x as i32, y as i32), ch);
-            if ch == LANTERNFISH {
-                location = (x as i32, y as i32);
-            }
-        }
+    let (mut warehouse, mut location) = get_warehouse(segments.next().unwrap());
+    let directions = get_directions(segments.next().unwrap());
+    for direction in &directions {
+        location = try_move(&mut warehouse, &location, &direction, true);
     }
+    println!("Part 1: {}", gps(&warehouse));
 
-    let directions: Vec<Point> = segments
-        .next()
-        .unwrap()
-        .chars()
-        .filter(|c| c != &'\n')
-        .map(|ch| get_direction(ch))
-        .collect();
+    segments = contents.split("\n\n");
+    (warehouse, location) = get_warehouse(&expand(segments.next().unwrap()));
 
-    for direction in directions {
-        location = try_move(&mut warehouse, &location, &direction);
+    for direction in &directions {
+        location = try_move(&mut warehouse, &location, &direction, true);
     }
-
-    let part_1: i32 = warehouse.iter().map(|((x, y), ch)| {
-        if *ch == BOX {
-            100 * y + x
-        } else {
-            0
-        }
-    }).sum();
-
-    println!("Part 1: {}", part_1);
+    println!("Part 2: {}", gps(&warehouse));
 }
