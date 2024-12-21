808eafb07c772de26a3d66976dbcfb99d704cd56
bgaudino <bgaudino@gmail.com>
2024-12-20 22:00:39 -0600

    Day 20 part 2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,11 +1,7 @@
-use std::{
-    collections::{HashMap, HashSet},
-    f32::INFINITY,
-    fs,
-};
+use std::{collections::HashSet, fs};
 
 type Point = (i32, i32);
-type State = (Point, usize);
+
 struct RaceTrack {
     walls: HashSet<Point>,
     max_x: i32,
@@ -19,34 +15,27 @@ impl RaceTrack {
         point.0 < 0 || point.0 > self.max_x || point.1 < 0 || point.1 > self.max_y
     }
 
-    fn bfs(&mut self, best_time: usize, brick: Option<Point>) -> Option<usize> {
+    fn find_track(&mut self) -> Option<Vec<Point>> {
         let mut visited: HashSet<Point> = HashSet::new();
-        let mut queue: Vec<State> = vec![(self.start, 0)];
-        let mut route_costs: Vec<usize> = Vec::new();
-        let mut walls = self.walls.clone();
-        if let Some(brick) = brick {
-            walls.remove(&brick);
-        }
+        let mut queue: Vec<(Point, Vec<Point>)> = vec![(self.start, vec![self.start])];
         while !queue.is_empty() {
-            let (point, steps) = queue.remove(0);
+            let (point, path) = queue.remove(0);
             if visited.contains(&point) {
                 continue;
             }
             visited.insert(point);
-            if steps > best_time {
-                continue;
-            }
             if point == self.finish {
-                route_costs.push(steps);
-                return Some(steps);
+                return Some(path);
             }
             let (x, y) = point;
             let next_points = vec![(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)];
             for next_point in next_points {
-                if self.is_out_of_bounds(next_point) || walls.contains(&point) {
+                if self.is_out_of_bounds(next_point) || self.walls.contains(&point) {
                     continue;
                 }
-                queue.push((next_point, steps + 1));
+                let mut new_path = path.clone();
+                new_path.push(next_point);
+                queue.push((next_point, new_path));
             }
         }
         return None;
@@ -87,16 +76,35 @@ fn get_race_track(s: &str) -> RaceTrack {
     }
 }
 
+fn manhattan_distance(p1: Point, p2: Point) -> i32 {
+    let (x1, y1) = p1;
+    let (x2, y2) = p2;
+    (x1 - x2).abs() + (y1 - y2).abs()
+}
+
 fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
     let mut race_track = get_race_track(&contents);
-    let steps = race_track.bfs(INFINITY as usize, None).unwrap();
-    let mut counter: HashMap<usize, usize> = HashMap::new();
-    for brick in race_track.walls.clone() {
-        if let Some(cheat) = race_track.bfs(steps - 100, Some(brick)) {
-            *counter.entry(steps - cheat).or_insert(0) += 1;
+    let track = race_track.find_track().unwrap();
+
+    let mut part_1 = 0;
+    let mut part_2 = 0;
+    let min_savings = 100;
+    for i in 0..track.len() - min_savings {
+        for j in i + min_savings + 1..track.len() {
+            let (p1, p2) = (track[i], track[j]);
+            let distance = manhattan_distance(p1, p2) as usize;
+            let savings = j - i - distance;
+            if savings >= min_savings {
+                if distance == 2 {
+                    part_1 += 1;
+                }
+                if distance <= 20 {
+                    part_2 += 1;
+                }
+            }
         }
     }
-    let part_1: usize = counter.iter().map(|(_, v)| v).sum();
     println!("Part 1: {}", part_1);
+    println!("Part 2: {}", part_2);
 }
